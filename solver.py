87a4c6cdcc62607650790096232e35c980a91166
Aaron Saban <aaron.saban@bennu.tv>
2019-09-09 18:04:57 -0300

    adding solve method 9x9

--- solver.py
+++ solver.py
@@ -33,6 +33,43 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def valid(board, number, position):
+
+    # Check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+    # Check column
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+    #Check box
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return board
+    else:
+        row, column = find
+    
+    for i in range(1,10):
+        if valid(board, i, (row, column)):
+            board[row][column] = i
+
+            if solve(board):
+                return solve(board)
+            board[row][column] = 0
+    
+    return False
+
 def print_board(board):
     """ Print a board 9x9  """
     for i in range(len(board)):
@@ -58,5 +95,6 @@ def find_empty(board):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j)
+    return None
 
 print_board(board)
