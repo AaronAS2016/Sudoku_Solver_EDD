b84724142082c39b494e39618c675599d4d14a10
Aaron Saban <aaron.saban@bennu.tv>
2019-09-11 04:23:48 -0300

    complete todo reader.py

--- reader.py
+++ reader.py
@@ -1,8 +1,11 @@
 #TODO: 
 # [] Leer archivo
-# [] Leer csv
-# [] Excepciones
+# [X] Leer csv
+# [X] Excepciones
+# [] Completar docs
 
+import os
+import csv
 
 class Reader():
     """ 
@@ -28,12 +31,61 @@ class Reader():
     
     
      """
-    def __init__(self, filepath=""):
+    def __init__(self, filepath="", delimiter=";"):
         self._filepath = filepath
+        self._delimiter = delimiter
 
+    
+    def setDelimiter(self, delimiter):
+        self._delimiter = delimiter
+    
     def setFile(self, filepath):
-        ''''@param   '''
+        """ 
+        Change the Path of the class
+
+        Parameter
+        --------
+    
+        filepath: str 
+            the new path of the file that want to read
+        
+        """
         self._filepath = filepath
 
+    def checkIfExistFile(self):
+        return os.path.exists(self._filepath)
+
+    def readFileAsCSV(self, delimiter=None):
+
+        # Use the default delimiter
+        if delimiter is None:
+            delimiter = self._delimiter
+
+        if(self.checkIfExistFile()):
+
+            try:
+                with open(self._filepath, 'r') as readerCsv:
+                    reader = csv.reader(readerCsv, delimiter=delimiter)
+                    data = []
+                    for row in reader:
+                        data.append(row)
+
+            except ValueError:
+                print("Error trying to read the file: " + ValueError)
+            return data
+        else:
+            print('Ups!, The filepath looks wrong, try to change the filepath')
+
+## Test caseros 
+
+# Caso bueno
+lector = Reader()
+lector.setFile('resources/boards.csv')
+print(lector.readFileAsCSV(','))
+
+#Caso Malo
+lector2 = Reader()
+lector2.setFile('rutaquenoexisteniagancho')
+print(lector2.readFileAsCSV())
 
     
