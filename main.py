a8a589f75b1b5c5b5c23b9e5e3589162f5facba3
Aaron Saban <aaron.saban@bennu.tv>
2019-09-16 17:38:33 -0300

    agregado escritura de soluciones

--- main.py
+++ main.py
@@ -2,20 +2,31 @@
 
 from solver import Solver
 from reader import Reader
-import menu
+from writer import Writer
+
+#import menu
 import os
 import time
+import utils
 
 reader = Reader('resources/boards.csv', ',')
-
+writter = Writer()
 board = reader.readFileAsCSV()
 
 sudoku = Solver()
 
 os.system('clear')
-# Un fix rapido por que los elementos que trae el reader, estan en formato Strings, ver donde ponemos esta conversion
 
+# Un fix rapido por que los elementos que trae el reader, estan en formato Strings, ver donde ponemos esta conversion
 board_fixed = [[int(num) for num in sub] for sub in board]
+
+
+def saveResultsInCsv(data, filepath="resources/solve.csv"):
+    os.system('clear')
+    print("Guardandooo...")
+    time.sleep(2)
+    writter.writeAsCSV(filepath, data)
+    print("Guardado!")
 
 
 def printMenu():
@@ -26,14 +37,49 @@ def printMenu():
 
 
 def solverMode():
-    pass
+    print("Bienvenido al modo solver")
+    pathFile = input(
+        "Ingrese el path del archivo o 0 para volver al menu principal: ")
+
+    while pathFile != "0":
+
+        reader.setFile(pathFile)
+        board = reader.readFileAsCSV()
+
+        os.system('clear')
+        print("\n")
+
+        print("El sudoku que ingreso es ...")
+        print("\n")
+
+        sudoku.print_board(board)
+        print("\n")
+        print("=========Solucion============")
+
+        sudoku_solved = sudoku.solve(board)
+
+        sudoku.print_board(sudoku_solved)
+        print("\n")
+        print("\n")
+
+        saveResults = input(
+            "¿Desea guardar los resultados en un archivo csv? (Y/N)")
+        if saveResults == 'Y':
+            filepathResult = input(
+                "¿Donde desea guardar los resultados? Ingrese el path: ")
+            saveResultsInCsv(sudoku_solved, filepathResult)
+
+        pathFile = input(
+            "Ingrese el path del archivo o 0 para volver al menu principal: ")
 
 
 answer = 0
 while(answer != "4"):
+    os.system('clear')
     printMenu()
     answer = input("Ingrese la opcion que quiera ejecutar: ")
     if answer == "1":
+        os.system('clear')
         solverMode()
 
 
