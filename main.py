9efb27603a102f533c314702156725c06dc38604
Aaron Saban <aaron.saban@bennu.tv>
2019-09-18 14:11:49 -0300

    solver 9x9

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 
 
+import utils
+import time
+import os
+
 from solver import Solver
 from reader import Reader
 from writer import Writer
 
-#import menu
-import os
-import time
-import utils
 
-reader = Reader('resources/boards.csv', ',')
+# import menu
+
+reader = Reader()
+
 writter = Writer()
-board = reader.readFileAsCSV()
 
 sudoku = Solver()
 
 os.system('clear')
 
 # Un fix rapido por que los elementos que trae el reader, estan en formato Strings, ver donde ponemos esta conversion
-board_fixed = [[int(num) for num in sub] for sub in board]
+# board_fixed = [[int(num) for num in sub] for sub in board]
 
 
 def saveResultsInCsv(data, filepath="resources/solve.csv"):
@@ -36,6 +38,40 @@ def printMenu():
     print("4 - Salir")
 
 
+def solveBoard(board):
+    print("El sudoku a solucionar es ...")
+    print("\n")
+    sudoku.print_board(board)
+    print("\n")
+    print("=========Solucion============")
+
+    sudoku_solved = sudoku.solve(board)
+
+    sudoku.print_board(sudoku_solved)
+    print("\n")
+    print("\n")
+
+    return sudoku_solved
+
+
+def splitBoards(boards):
+
+    lista = []
+
+    lista_a_agregar = []
+
+    contador = 0
+
+    for fila in boards:
+        contador += 1
+        lista_a_agregar.append(list(map(int, fila)))
+        if contador == 9:
+            lista.append(lista_a_agregar)
+            lista_a_agregar = []
+            contador = 0
+    return lista
+
+
 def solverMode():
     print("Bienvenido al modo solver")
     pathFile = input(
@@ -49,25 +85,26 @@ def solverMode():
         os.system('clear')
         print("\n")
 
-        print("El sudoku que ingreso es ...")
-        print("\n")
+        if type(board) is not list:
+            print("Tablero invalido, Por favor ponga otra ruta")
+        else:
+            sudoku_solved = []
 
-        sudoku.print_board(board)
-        print("\n")
-        print("=========Solucion============")
+            if len(board) > 9:
+                boards = splitBoards(board)
+                for newboard in boards:
+                    sudoku_solved += solveBoard(newboard)
+                # print(splitBoards(board))
 
-        sudoku_solved = sudoku.solve(board)
+            else:
+                sudoku_solved += solveBoard(board)
 
-        sudoku.print_board(sudoku_solved)
-        print("\n")
-        print("\n")
-
-        saveResults = input(
-            "¿Desea guardar los resultados en un archivo csv? (Y/N)")
-        if saveResults == 'Y':
-            filepathResult = input(
-                "¿Donde desea guardar los resultados? Ingrese el path: ")
-            saveResultsInCsv(sudoku_solved, filepathResult)
+            saveResults = input(
+                "¿Desea guardar los resultados en un archivo csv? (Y/N)")
+            if saveResults == 'Y':
+                filepathResult = input(
+                    "¿Donde desea guardar los resultados? Ingrese el path: ")
+                saveResultsInCsv(sudoku_solved, filepathResult)
 
         pathFile = input(
             "Ingrese el path del archivo o 0 para volver al menu principal: ")
