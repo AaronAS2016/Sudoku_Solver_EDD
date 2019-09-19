82095f46d20f9bd2ae9834cb7146c93bb99ecf7f
Aaron Saban <aaron.saban@bennu.tv>
2019-09-19 15:40:57 -0300

    solver nxn y timer

--- solver.py
+++ solver.py
@@ -1,6 +1,6 @@
 class Solver():
 
-    def valid(self, board, number, position):
+    def valid(self, board, number, position, dimension=9):
 
         # Check row
         for i in range(len(board[0])):
@@ -10,48 +10,49 @@ class Solver():
         for i in range(len(board)):
             if board[i][position[1]] == number and position[0] != i:
                 return False
-        #Check box
+        # Check box
         box_x = position[1] // 3
         box_y = position[0] // 3
 
-        for i in range(box_y * 3, box_y * 3 + 3):
-            for j in range(box_x * 3, box_x * 3 + 3):
+        for i in range(box_y * 3, box_y * 3 + (dimension // 3)):
+            for j in range(box_x * 3, box_x * 3 + (dimension // 3)):
                 if board[i][j] == number and (i, j) != position:
                     return False
         return True
 
-    def solve(self, board, dimension = 9):
+    def solve(self, board, dimension=9):
         find = self.find_empty(board)
-        
+
         if not find:
             return board
         else:
             row, column = find
-        
+
         for i in range(1, dimension + 1):
-            if self.valid(board, i, (row, column)):
+            if self.valid(board, i, (row, column), dimension):
                 board[row][column] = i
 
-                if self.solve(board):
-                    return self.solve(board)
+                solucion = self.solve(board)
+                if solucion:
+                    return solucion
                 board[row][column] = 0
-        
+
         return False
 
     def print_board(self, board):
         """ Print a board 9x9  """
         for i in range(len(board)):
-            #Every 3 rows print a separator
+            # Every 3 rows print a separator
             if i % 3 == 0 and i != 0:
                 print("- - - - - - - - - - - -")
 
             for j in range(len(board[0])):
 
-                #Every 3 column print a separator
+                # Every 3 column print a separator
                 if j % 3 == 0 and j != 0:
                     print(" | ", end="")
 
-                #Check if is last column to make the jump line
+                # Check if is last column to make the jump line
                 if j == 8:
                     print(board[i][j])
                 else:
